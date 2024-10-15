26a49a2cdb8980f22920b787e34565a5747f43f9
CodeWithMayank-Py <paliwalm4321@gmail.com>
2024-10-15 13:01:12 +0530

    Creating Simple API

--- app.py
+++ app.py
@@ -3,6 +3,55 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+# Dummy data to simulate resources
+users = [
+    {"id":1, "name": "John Smith"},
+    {"id":2, "name": "Pirate Jacky"},
+]
+
+tasks = [
+    {"id":1, "title": "Working on Daily Reports", "description": "Day 1"},
+    {"id":2, "title": "Sailing the ship", "description": "Roaming around the world"},
+]
+
+# Homepage
+@app.route('/')
+def homepage():
+    return "Hello, Developers! Welcome to simple flask APIs."
+
+# Get fetch all users using GET method
+@app.route('/users', methods=['GET'])
+def get_users():
+    return jsonify(users)
+
+# POST: Create a new task
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    new_task = request.json
+    new_task['id'] = len(tasks)+ 1
+    tasks.append(new_task)
+    return jsonify(new_task), 201 # 201: Task Creation
+
+# PUT: Update an existing Task
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    updated_task = request.json
+    for task in tasks:
+        if task['id'] == task_id:
+            task.update(updated_task)
+            return jsonify(task), 200 # 200: Successfully Updated
+    return jsonify({"error": "Task not found"}), 404 # 404: Error Occured on Client Side
+
+# DELETE: Delete a task
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    delete_task = request.json
+    for task in tasks:
+        if task['id'] == task_id:
+            pass
+            
+    
+
 
 
 if __name__ == '__main__':
